9e6cdb77da19352284cdb85b04d69ada09750890
Priyanshu Jain <priyanshu0405jain@gmail.com>
2021-06-14 17:32:59 +0530

    add settings ui

--- src/Pages/Settings/Web.elm
+++ src/Pages/Settings/Web.elm
@@ -2,8 +2,9 @@ module Pages.Settings.Web exposing (Model, Msg, Params, page)
 
 import Colors
 import Components.VerticalNavSettings
-import Element exposing (column, fill, fillPortion, row, spacingXY)
+import Element exposing (..)
 import Element.Background as Background
+import Element.Font as Font
 import Spa.Document exposing (Document)
 import Spa.Generated.Route exposing (Route)
 import Spa.Page as Page exposing (Page)
@@ -65,9 +66,13 @@ view : Model -> Document Msg
 view model =
     { title = "Settings.Web"
     , body =
-        Components.VerticalNavSettings.view
-            model.route
-            ++ [ column [ Element.height fill, Element.width (fillPortion 6), spacingXY 5 7, Background.color Colors.background ]
-                    []
-               ]
+        [
+          row[]
+          [ column[](Components.VerticalNavSettings.view
+              model.route)
+          , column[][
+          el[width (fillPortion 1), Font.color (rgb255 18 178 231), Font.size 24, Font.light, paddingXY 5 1](text "General options")
+          ]
+          ]
+        ]
     }
